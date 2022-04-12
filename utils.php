46cc5c714b642e2c7a185c839680b07bffcee3b0
albertodiaz <contacto@albertodiazsaez.com>
2022-04-12 13:23:08 +0200

    tarea

--- utils.php
+++ utils.php
@@ -3,14 +3,14 @@
 function printHeader($printer, $decodedJson)
 {
 
-    $printer->setReverseColors(true);
     $timestamp = $decodedJson->message->date;
     $timezone = "Europe/Madrid";
     $dt = new DateTime();
     $dt->setTimestamp($timestamp);
     $dt->setTimezone(new DateTimeZone($timezone));
     $datetime = $dt->format('Y-m-d H:i:s');
-
+    
+    $printer->setReverseColors(true);
     $printer->text($decodedJson->message->chat->first_name . ' - ' . $datetime);
     $printer->feed(2);
     $printer->setReverseColors(false);
